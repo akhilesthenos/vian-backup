39656f132b1a4228ac00ef03e6d651e2f6937b4e
akhilesthenos <akhil@esthenos.com>
2022-06-07 11:37:48 +0530

    first commit

---
+++ src/pages/career.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ScrollToTop from "../components/scroll-to-top";
+import SEO from "../components/seo";
+import ContactContainer from "../containers/contact";
+import NewsletterArea from "../containers/global/newsletter";
+import RoadMap from "../containers/home/roadmap1";
+import PageBanner from "../containers/global/page-banner";
+import Footer from "../layouts/footer";
+import Header from "../layouts/header";
+import Layout from "../layouts/index";
+
+const CareerPage = () => {
+    return (
+        <React.Fragment>
+            <Layout>
+                <SEO title="VIAN – Contact" />
+                <div className="wrapper">
+                    <Header />
+                    <PageBanner
+                        title="Join Us"
+                        excerpt="We offer you the opportunity to solve some of 
+                        the most interesting challenges our society have which 
+                        will be equally compensated by our team-driven culture 
+                        and rewards for individual talent. Send your profile at 
+                        career@viancapital.com"
+                        image="./images/career/1.png"
+                    />
+                    {/* <RoadMap /> */}
+                    {/* <ContactContainer />
+                    <NewsletterArea /> */}
+                    <Footer />
+                    <ScrollToTop />
+                </div>
+            </Layout>
+        </React.Fragment>
+    );
+};
+
+export default CareerPage;
